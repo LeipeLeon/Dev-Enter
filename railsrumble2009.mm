f13123b87b0f2e38fa7cba7f30c9ce791f230989
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2009-10-31 10:21:32 +0100

    first version of mindmap

---
+++ railsrumble2009.mm
@@ -0,0 +1,320 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1256975112267" ID="Freemind_Link_1794804172" MODIFIED="1256975123375" TEXT="Rails app dependencies">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1256975125915" ID="_" MODIFIED="1256975131012" POSITION="right" TEXT="what">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256975131480" ID="Freemind_Link_246414978" MODIFIED="1256975132612" TEXT="gems">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975702596" ID="Freemind_Link_809557203" MODIFIED="1256975706088" TEXT="rails etc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975707284" ID="Freemind_Link_1098452354" MODIFIED="1256975709448" TEXT="other gems">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256976379168" ID="Freemind_Link_1962249849" MODIFIED="1256976381220" TEXT="rake gems">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1256976381584" ID="Freemind_Link_1929463052" MODIFIED="1256976384836" TEXT="look at sourcecode"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975132904" ID="Freemind_Link_947558429" MODIFIED="1256975134268" TEXT="plugins">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975326818" ID="Freemind_Link_254090272" MODIFIED="1256975334758" TEXT="vendor/plugins">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975334970" ID="Freemind_Link_318522606" MODIFIED="1256975340173" TEXT="version">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975390482" ID="Freemind_Link_346843575" MODIFIED="1256975392478" TEXT="database">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975409514" ID="Freemind_Link_1570635704" MODIFIED="1256975414014" TEXT="database.yml">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975444850" ID="Freemind_Link_1298617033" MODIFIED="1256975446790" TEXT="source control">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975430858" ID="Freemind_Link_130851915" MODIFIED="1256975449170" TEXT="git/svn">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975478651" ID="Freemind_Link_929346657" MODIFIED="1256975483369" TEXT="external source">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975484171" ID="Freemind_Link_1047314323" MODIFIED="1256975495918" TEXT="when build native extensions">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975647332" ID="Freemind_Link_194660898" MODIFIED="1256975649191" TEXT="ruby">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975653580" ID="Freemind_Link_1479792653" MODIFIED="1256975656503" TEXT="1.8.6">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975656748" ID="Freemind_Link_789795475" MODIFIED="1256975659815" TEXT="1.8.7">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975660036" ID="Freemind_Link_1607872903" MODIFIED="1256975662224" TEXT="1.9.2">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975786780" ID="Freemind_Link_649575220" MODIFIED="1256975788120" TEXT="rubygems">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256975173137" ID="Freemind_Link_1662848983" MODIFIED="1256975174693" POSITION="left" TEXT="onderzoek">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256975139809" ID="Freemind_Link_1358341683" MODIFIED="1256975176705" TEXT="existing tools for extraction">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975167953" ID="Freemind_Link_143004408" MODIFIED="1256975176705" TEXT="zoeken">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975312826" ID="Freemind_Link_352660124" MODIFIED="1256975317717" TEXT="rake gems:unpack:dependencies">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256975803725" ID="Freemind_Link_1674280858" MODIFIED="1256975806000" POSITION="left" TEXT="stage">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256975806636" ID="Freemind_Link_1146616864" MODIFIED="1256975808056" TEXT="development">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256975808461" ID="Freemind_Link_657210892" MODIFIED="1256975811064" TEXT="test">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256975814749" ID="Freemind_Link_1990700239" MODIFIED="1256975815984" TEXT="production">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256975832069" ID="Freemind_Link_177433890" MODIFIED="1256975836641" POSITION="right" TEXT="Depencency">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256975837141" ID="Freemind_Link_1764937018" MODIFIED="1256975841688" TEXT="type">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975841981" ID="Freemind_Link_1329778052" MODIFIED="1256975844168" TEXT="gem">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1256975866173" ID="Freemind_Link_774424284" MODIFIED="1256975869705" TEXT="rubygems"/>
+</node>
+<node COLOR="#990000" CREATED="1256975844789" ID="Freemind_Link_852286887" MODIFIED="1256975846041" TEXT="plugin">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975846301" ID="Freemind_Link_1510221569" MODIFIED="1256975849537" TEXT="database">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975849773" ID="Freemind_Link_1513088266" MODIFIED="1256975851417" TEXT="source control">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975851644" ID="Freemind_Link_1379872773" MODIFIED="1256975858824" TEXT="external (source)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975859029" ID="Freemind_Link_318583117" MODIFIED="1256975860601" TEXT="ruby">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975879021" ID="Freemind_Link_1387034426" MODIFIED="1256975885337" TEXT="environment">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256975900749" ID="Freemind_Link_1876265702" MODIFIED="1256975901489" TEXT="test">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975890373" ID="Freemind_Link_264902613" MODIFIED="1256975896281" TEXT="development">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975907213" ID="Freemind_Link_987469261" MODIFIED="1256975908033" TEXT="staging">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975904189" ID="Freemind_Link_1081926255" MODIFIED="1256975906353" TEXT="production">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256975956445" ID="Freemind_Link_99057292" MODIFIED="1256975962073" TEXT="3 BITS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256975964758" ID="Freemind_Link_1999405672" MODIFIED="1256975969353" TEXT="acts_as_tree">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256975984294" ID="Freemind_Link_550029259" MODIFIED="1256975985857" TEXT="application">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256977190805" ID="Freemind_Link_1814457035" MODIFIED="1256977192537" TEXT="version">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256977198501" ID="Freemind_Link_1286826177" MODIFIED="1256977201398" TEXT="name">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256977280941" ID="Freemind_Link_1913310396" MODIFIED="1256977282961" TEXT="location">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256977283333" ID="Freemind_Link_604735292" MODIFIED="1256977284977" TEXT="installed">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977285405" ID="Freemind_Link_1139535398" MODIFIED="1256977287721" TEXT="frozen">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1256977337198" ID="Freemind_Link_1219192612" MODIFIED="1256977339129" TEXT="vendored"/>
+</node>
+<node COLOR="#990000" CREATED="1256977288229" ID="Freemind_Link_1666225145" MODIFIED="1256977289737" TEXT="framework">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977311045" ID="Freemind_Link_1916749177" MODIFIED="1256977316433" TEXT="[nil]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256977377366" ID="Freemind_Link_88347198" MODIFIED="1256977399562" TEXT="home_url">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256977384638" ID="Freemind_Link_646127294" MODIFIED="1256977388033" TEXT="github url">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977389158" ID="Freemind_Link_1803135550" MODIFIED="1256977393970" TEXT="rubyforge">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977402814" ID="Freemind_Link_1785009700" MODIFIED="1256977404914" TEXT="gemcutter">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256979120111" ID="Freemind_Link_1360323433" MODIFIED="1256979126667" TEXT="(trusted source)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256977506503" ID="Freemind_Link_395318626" MODIFIED="1256977513698" TEXT="forks">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256978397595" ID="Freemind_Link_524383560" MODIFIED="1256978404911" TEXT="related websites">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256978405395" ID="Freemind_Link_47846146" MODIFIED="1256978406175" TEXT="blog">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256978406932" ID="Freemind_Link_717478169" MODIFIED="1256978457944" TEXT="github address">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256979000055" ID="Freemind_Link_258853183" MODIFIED="1256979006226" TEXT="state">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256979006503" ID="Freemind_Link_739352946" MODIFIED="1256979007875" TEXT="stable">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256979008391" ID="Freemind_Link_1505340013" MODIFIED="1256979011595" TEXT="alpha">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256979011847" ID="Freemind_Link_1046039471" MODIFIED="1256979012731" TEXT="beta">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256979025103" ID="Freemind_Link_1425350667" MODIFIED="1256979028083" TEXT="known problems">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256979039407" ID="Freemind_Link_1470667019" MODIFIED="1256979057939" TEXT="issue_tracker_url">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256976392600" ID="Freemind_Link_578736060" MODIFIED="1256976394236" POSITION="left" TEXT="App">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256977202349" ID="Freemind_Link_369601988" MODIFIED="1256977204689" TEXT="name">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256976595369" ID="Freemind_Link_1503087290" MODIFIED="1256976599613" POSITION="right" TEXT="concept">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256976614721" ID="Freemind_Link_1828473411" MODIFIED="1256976651926" TEXT="vergelijking tussen huidige situatie en nieuw beschikbare gems">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256976675130" ID="Freemind_Link_1009126750" MODIFIED="1256976679485" TEXT="op basis van github url">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256977571038" ID="Freemind_Link_718305764" MODIFIED="1256977594290" TEXT="rebase when push has been merged">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256978746150" ID="Freemind_Link_897033845" MODIFIED="1256978760546" TEXT="push dependency informatie naar webservice a la Wakoopa">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256978761189" ID="Freemind_Link_1628154295" MODIFIED="1256978778321" TEXT="pagina per app">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256978778597" ID="Freemind_Link_1242887074" MODIFIED="1256978784289" TEXT="pagina per dependency">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256976717842" ID="Freemind_Link_378135307" MODIFIED="1256976719278" POSITION="left" TEXT="basis">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256976719866" ID="Freemind_Link_1067631024" MODIFIED="1256976722726" TEXT="draaiende applicatie">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256976723282" ID="Freemind_Link_1545894191" MODIFIED="1256976729862" TEXT="suburl a la /newrelic">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256976742226" ID="Freemind_Link_1839143671" MODIFIED="1256976745118" TEXT="github url">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256976745386" ID="Freemind_Link_1851993568" MODIFIED="1256976749478" TEXT="clone + check">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256977164477" ID="Freemind_Link_1397301363" MODIFIED="1256977167888" POSITION="right" TEXT="voorbeeld app">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256975156169" ID="Freemind_Link_1157950717" MODIFIED="1256977179448" TEXT="Thoughtbot Suspenders">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256977158413" ID="Freemind_Link_1808095954" MODIFIED="1256977171005" TEXT="rails template">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256977179685" ID="Freemind_Link_1344052158" MODIFIED="1256977181552" TEXT="bort">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256977855472" ID="Freemind_Link_758231758" MODIFIED="1256977859372" POSITION="right" TEXT="componenten">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256977859537" ID="Freemind_Link_1348477908" MODIFIED="1256977864661" TEXT="dependency extractor">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256977866097" ID="Freemind_Link_1270809024" MODIFIED="1256977871988" TEXT="look at rake gems">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977874873" ID="Freemind_Link_1616066169" MODIFIED="1256977878444" TEXT="plugins? how">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977880201" ID="Freemind_Link_173619623" MODIFIED="1256977885780" TEXT="database.yml">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1256977888649" ID="Freemind_Link_224571763" MODIFIED="1256977905924" TEXT="rails version">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256978944655" ID="Freemind_Link_1490978295" MODIFIED="1256978961202" TEXT="new versions?">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256979081039" ID="Freemind_Link_550499660" MODIFIED="1256979088235" TEXT="viewing info">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256979088751" ID="Freemind_Link_1008762942" MODIFIED="1256979091987" TEXT="link to external sources">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256979093159" ID="Freemind_Link_1544738756" MODIFIED="1256979099477" TEXT="spider/colllect external info">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256979309672" ID="Freemind_Link_1887501752" MODIFIED="1256979318140" TEXT="rake task to extract dependencies">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256979318345" ID="Freemind_Link_1225983344" MODIFIED="1256979323325" TEXT="push info to webservice">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1256978126394" ID="Freemind_Link_1296701889" MODIFIED="1256978126398" POSITION="left" TEXT="">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1256980453216" ID="Freemind_Link_364449502" MODIFIED="1256980454803" POSITION="right" TEXT="steps">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1256980455103" ID="Freemind_Link_723294970" MODIFIED="1256980458580" TEXT="gem install unravel">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256980458895" ID="Freemind_Link_1962178029" MODIFIED="1256980465507" TEXT="unravel create &apos;myapp&apos;">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1256980466399" ID="Freemind_Link_1494962957" MODIFIED="1256980571747" TEXT="(unravel)">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256980572008" ID="Freemind_Link_675520507" MODIFIED="1256980573180" TEXT="optional">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1256980471031" ID="Freemind_Link_143478343" MODIFIED="1256980473499" TEXT="unravel push">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1256980474327" ID="Freemind_Link_387976323" MODIFIED="1256980498198" TEXT="creates account">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1256980578704" ID="Freemind_Link_1998839938" MODIFIED="1256980580315" TEXT="if needed"/>
+</node>
+</node>
+</node>
+</node>
+</map>
