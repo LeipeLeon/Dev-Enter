195fdcdf4c282633022ed8d7c8e69b42964a8424
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2009-10-31 13:09:12 +0100

    extracts unified package dependencies

--- gem/unravel/lib/unravel/extractor.rb
+++ gem/unravel/lib/unravel/extractor.rb
@@ -6,84 +6,90 @@ module Unravel
       @options = options
     end
   end
-  
-  class Dependency
-    dep = {
-      :frozen => true,
-      :lib => 'capistrano',
-      :name => 'capistrano-ext',
-      :source => 'http://gems.github.com',
-      :spec => {
-        :name => 'capistrano-ext',
-        :version => {
-          :version => '1.0.0',
-        },
-        :platform => 'ruby',
 
-        :dependencies => {
-          :name => 'test',
-          :version_requirement => {
-            :conditions => ">=",
-            :version => {
-              :version => '1.0.0'
-            }
-          }
-        },
-        :description => '',
-        :authors => [ 'Jamis Buck' ],
-        :exectuables => [],
-        :homepage => 'http://www.capify.org',
-        :required_ruby_version => {
-          :requirements => {
-            :version => {
-              :conditions => ">=",
-              :version => {
-                :version => '1.0.0'
-              }
-            }
-          }
-        },
-        :rubyforge_project => 'capistrano-ext',
-        :rubygems_version => '1.3.5',
-        :summary => 'Useful task libraries and methods for Capistrano',
-      },
-      
-    }
-  end
+  # # useful attributes from Gem::Dependency tree
+  # dep = {
+  #   :frozen => true,
+  #   :lib => 'capistrano',
+  #   :name => 'capistrano-ext',
+  #   :source => 'http://gems.github.com',
+  #   :spec => {
+  #     :name => 'capistrano-ext',
+  #     :version => {
+  #       :version => '1.0.0',
+  #     },
+  #     :platform => 'ruby',
+  # 
+  #     :dependencies => {
+  #       :name => 'test',
+  #       :version_requirement => {
+  #         :conditions => ">=",
+  #         :version => {
+  #           :version => '1.0.0'
+  #         }
+  #       }
+  #     },
+  #     :description => '',
+  #     :authors => [ 'Jamis Buck' ],
+  #     :exectuables => [],
+  #     :homepage => 'http://www.capify.org',
+  #     :required_ruby_version => {
+  #       :requirements => {
+  #         :version => {
+  #           :conditions => ">=",
+  #           :version => {
+  #             :version => '1.0.0'
+  #           }
+  #         }
+  #       }
+  #     },
+  #     :rubyforge_project => 'capistrano-ext',
+  #     :rubygems_version => '1.3.5',
+  #     :summary => 'Useful task libraries and methods for Capistrano',
+  #   },
+  # }
   
   class GemExtractor< Extractor
     
     def run
+      # load rails to get the gem dependencies
       require File.join('config', 'environment')
 
-      # do stuff
-      gems = Rails.configuration.gems
-      puts gems.to_yaml
-      dependencies = gems.inject([]) do |memo, gem|
-        memo << {
-          :name        => gem.name,
-          :source      => gem.source,
-          :lib         => gem.lib,
-          # :version     => gem.spec.version.version,
-          # :platform    => gem.spec.platform,
-          # :description => gem.spec.description,
-          # :authors     => gem.spec.authors,
-          # :homepage    => gem.spec.homepage,
-          # :rubyforge_project => gem.spec.rubyforge_project,
-          # :summary     => gem.spec.summary,
-        }
+      # gems = Rails.configuration.gems
+      # puts gems.to_yaml
+
+      packages = Rails.configuration.gems.map do |gem|
+        unravel(gem)
       end
-      puts dependencies.to_yaml
-      # each do |gem|
-      #   print_gem_status(gem)
-      # end
-      # puts
-      # puts "I = Installed"
-      # puts "F = Frozen"
-      # puts "R = Framework (loaded before rails starts)"
+
+      packages
     end
     
     private
+    
+    #
+    # recursively extract useful attributes from the gems
+    #
+    def unravel(gem, level=1)
+      spec = gem.specification
+      {
+        :package => {
+          :type              => 'gem', # possible values %w(gem binary plugin database scm)
+          :name              => gem.name,
+          :source            => gem.source,
+          :lib               => gem.lib,
+          :version           => spec.version.to_s,
+          :platform          => spec.platform,
+          :description       => spec.description,
+          :authors           => spec.authors,
+          :homepage          => spec.homepage,
+          :rubyforge_project => spec.rubyforge_project,
+          :summary           => spec.summary,
+          :required_ruby_version => spec.required_ruby_version.to_s,
+          :dependencies => gem.dependencies.map { |g| unravel(g, level+1) },
+        }
+      }
+    end
 
     def print_gem_status(gem, indent=1)
       code = case
@@ -98,12 +104,21 @@ module Unravel
   end
   
   class DatabaseExtractor < Extractor
+    def run
+      []
+    end
   end
   
   class PluginExtractor < Extractor
+    def run
+      []
+    end
   end
   
   class ScmExtractor < Extractor
+    def run
+      []
+    end
   end
   
   class RubyExtractor < Extractor
