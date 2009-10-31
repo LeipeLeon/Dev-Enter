c9ae177a3ea1d2a2b7515c687283bb12baab0070
Leon Berenschot <leonb@beriedata.nl>
2009-10-31 10:38:41 +0100

    Merge branch 'master' of github.com:LeipeLeon/Dev-Enter
    * 'master' of github.com:LeipeLeon/Dev-Enter:
      keep schema in git
      don't ignore database.yml
      database.yml in git for convenience
      reorganize

--- unravel/config/routes.rb
+++ unravel/config/routes.rb
@@ -1,8 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.resources :apps
+
+  map.logout  '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login    '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.signup   '/signup', :controller => 'users', :action => 'new'
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate'
   map.resources :users
 
   map.resource :session
@@ -39,7 +42,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "users"
 
   # See how all your routes lay out with "rake routes"
 
