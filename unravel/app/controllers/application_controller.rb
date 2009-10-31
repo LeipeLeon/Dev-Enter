01e6e236c3c806d87faa57575f3417ef128c6bfe
Leon Berenschot <leonb@beriedata.nl>
2009-10-31 10:35:12 +0100

    Added Auth Template

--- unravel/app/controllers/application_controller.rb
+++ unravel/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  # AuthenticatedSystem must be included for RoleRequirement, and is provided by installing acts_as_authenticates and running 'script/generate authenticated account user'.
+  include AuthenticatedSystem
+  # You can move this into a different controller, if you wish.  This module gives you the require_role helpers, and others.
+  include RoleRequirementSystem
+
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include ExceptionNotifiable
