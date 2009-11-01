6be4d86c427808813d93dd8159fd2f53281b18c4
Leon Berenschot <leonb@beriedata.nl>
2009-11-01 18:27:29 +0100

    Find latest version of package

--- unravel/app/models/package.rb
+++ unravel/app/models/package.rb
@@ -1,5 +1,24 @@
+require 'rest_client'
+require 'json'
+
 class Package < ActiveRecord::Base
-  has_many :versions
+  has_many :versions, :order => "version DESC"
   belongs_to :app_depends_on
   # belongs_to :app, :through => :app_depends_on
+  # named_scope :version, :order => {:verion => }
+  
+  def before_create
+    update_version
+  end
+
+  def update_version
+    if 'gem' == self.package_type
+      begin
+        @response = JSON.parse(RestClient.get("http://gemcutter.org/gems/#{self.name}.json"))
+        self.versions << Version.new(:version => @response["version"])
+      rescue RestClient::ResourceNotFound
+        "No version found for #{self.name}"
+      end
+    end
+  end
 end
