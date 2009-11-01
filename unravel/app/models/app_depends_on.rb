93b718158accb14bda302f5e049dcbef60bc0788
Leon Berenschot <leonb@beriedata.nl>
2009-11-01 18:28:50 +0100

    Correctly set AppDependsOn#package_id

--- unravel/app/models/app_depends_on.rb
+++ unravel/app/models/app_depends_on.rb
@@ -6,11 +6,12 @@ class AppDependsOn < ActiveRecord::Base
   
   def find_related(package)
     # find a matching package in our repo
-    Package.find_or_create_by_name_and_package_type_and_homepage_and_description(
+    package = Package.find_or_create_by_name_and_package_type_and_homepage_and_description(
       :name          => package[:name],
       :package_type  => package[:type],
       :homepage      => package[:homepage],
       :description   => package[:description]
     )
+    self.update_attribute(:package_id, package.id)
   end
 end
